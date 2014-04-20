2e547be63bad021e8d9ae495fa61d3165f036299
Jason B <devilshaircut@gmail.com>
2014-04-20 00:50:51 -0400

    Resolve some issues with the dynamic positioning of the page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,19 +49,24 @@ $(document).ready(function() {
   $("#ns-slide-controls").click(function() {
     $("body").toggleClass("ns-slider-expanded");
   });
-  // $("#ns-slide-controls").click(function() {
-  //   $("body").toggleClass("ns-slider-expanded");
-  // });
+  $("nav.ns-section-main ol#ns-navigation > li > a").click(function() {
+    $("body").removeClass("ns-slider-expanded");
+  });
+
+  // Make sure that if the user resizes, the dynamic states are also reset.
+  $(window).resize(function() {
+    $("body").removeClass("ns-slider-expanded");
+  });
 
   $(window).scroll(function() {
 
     // Show or hide the top bar background.
-    if ( $(window).scrollTop() > 0 ) {
-      $("nav.ns-section-main").addClass("ns-nav-shadow-state");
-    }
-    else if ( $(window).scrollTop() <= 0 ) {
-      $("nav.ns-section-main").removeClass("ns-nav-shadow-state");
-    }
+    // if ( $(window).scrollTop() > 0 ) {
+    //   $("nav.ns-section-main").addClass("ns-nav-shadow-state");
+    // }
+    // else if ( $(window).scrollTop() <= 0 ) {
+    //   $("nav.ns-section-main").removeClass("ns-nav-shadow-state");
+    // }
 
   });
 
